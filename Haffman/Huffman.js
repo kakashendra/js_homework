a4a93ad10d0e85f96ccb1f7452293b1a7580f6ca
Ekaterina Rogozinnikova <139217149+kakashendra@users.noreply.github.com>
2024-01-03 22:57:54 +0300

    Update Huffman.js

--- Haffman/Huffman.js
+++ Haffman/Huffman.js
@@ -1,32 +1,45 @@
-fs = require('fs')
-text = fs.readFileSync("input.txt", "utf-8");
-masiv = [];
-symbols = [];
-console.log(text)
-for (var i = 0; i < text.length; i++) {
-    if (!symbols[text[i]])
-        symbols[text[i]] = 0;
-    symbols[text[i]]++;
-}
-arr = [];
-for (i in symbols) {
-    arr.push([symbols[i], i]);
+let str = require('fs').readFileSync('text.txt');
+str = str.toString();
+
+let alph = new Object();
+let tree = new Array();
+
+for (i = 0 ; i < str.length; i++) {
+	if (!(str.charAt(i) in alph))
+        alph[str.charAt(i)] = 0;
+    alph[str.charAt(i)]++;
 }
 
-arr.sort();
-list = [];
-for (var i = 0; i < arr.length; i++) list[arr[i][1]] = "";
-
-while (arr.length > 1) {
-    arr.sort(function (a, b) {
-        if (a[0] === b[0]) return 0;        
-        return (a[0] < b[0]) ? -1 : 1;        
-    });
-    for (i = 0; i < arr[0][1].length; i++) list[arr[0][1][i]] = '0' + list[arr[0][1][i]];    
-    for (i = 0; i < arr[1][1].length; i++) list[arr[1][1][i]] = '1' + list[arr[1][1][i]];
-    arr[1][1] += arr[0][1];
-    arr[1][0] += arr[0][0];
-    arr = arr.slice(1);   
+for(ch in alph) {
+	let n = new Node(ch,undefined);
+	tree.push(n);
 }
-for (i in list) 
-    console.log(i, list[i], "in text:", symbols[i])
+
+tree.reverse();
+let lastCode = "";
+let encoded = new Object();
+let len = tree.length;
+first = tree.pop();
+first.code = "0";
+lastLetter = first.letter;
+encoded[first.letter] = first.code;
+lastCode = first.code;
+
+for(i = 1;i < len; i++) {
+	node = tree.pop();
+	if (i == len - 1)
+		node.code = '1' + lastCode.slice(0,-1);
+	else
+		node.code = "1" + lastCode;
+    encoded[node.letter] = node.code;
+	lastCode=node.code
+}
+let res = "";
+for (i=0 ; i < str.length ; i++)
+	res+=encoded[str[i]];
+console.log(res);
+
+function Node(letter, code) {
+	this.letter=letter;
+	this.code=code;
+}
